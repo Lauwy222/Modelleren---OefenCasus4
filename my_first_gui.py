2b75cd426eadd2d08d2f606a2b00e01ef17be517
Laurens van der Drift <Lauwy222@hotmail.com>
2026-01-15 16:49:44 +0100

    Add Matplotlib plot area to Tkinter GUI
    Integrated Matplotlib with the Tkinter application by creating a Figure and embedding it using FigureCanvasTkAgg. The plot area is now displayed on the right side of the GUI.

--- my_first_gui.py
+++ my_first_gui.py
@@ -7,6 +7,8 @@ Created on Thu Jan 15 16:38:41 2026
 
 #%% S1 - LIB
 import tkinter as tk 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 #%% S2 - Initial values
 
@@ -20,7 +22,11 @@ root.geometry("1000x750")
 root.resizable(False, False)
 root.title('My First GUI')
 
+figure1 = plt.Figure(figsize=(4,4), dpi=150)
+canvas = FigureCanvasTkAgg(figure1, master=root)
+canvas_widget = canvas.get_tk_widget()
 #%% S6 - Place GUI Elements
+canvas_widget.pack(side="right", fill="y")
 
 #%% S7- Create event loop
 root.mainloop()
