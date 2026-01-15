ece1175629477cdcd6cde9bf7ee8ecdcaaa92c0d
Laurens van der Drift <Lauwy222@hotmail.com>
2026-01-15 16:44:19 +0100

    Initialize basic Tkinter GUI structure
    Added import for tkinter and created the main window with specified geometry, non-resizable settings, and a title. Added section comments to organize future GUI development.

--- my_first_gui.py
+++ my_first_gui.py
@@ -5,3 +5,21 @@ Created on Thu Jan 15 16:38:41 2026
 @author: lauwy
 """
 
+#%% S1 - LIB
+import tkinter as tk 
+
+#%% S2 - Initial values
+
+#%% S3 - Gui Callback
+
+#%% S4 - Create model
+
+#%% S5 - Create GUI Elements
+root = tk.Tk()
+root.geometry("1000x750")
+root.resizable(False, False)
+root.title('My First GUI')
+
+#%% S6 - Place GUI Elements
+
+#%% S7- Create event loop
