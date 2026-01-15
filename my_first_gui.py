f3b02f8cee7bfa40315794fe196e267a75620130
Laurens van der Drift <Lauwy222@hotmail.com>
2026-01-15 16:56:47 +0100

    Add slider for joint angle to GUI
    Introduced a horizontal slider to the Tkinter GUI for adjusting the joint angle. The slider is placed at the bottom of the window, enhancing user interaction with the application.

--- my_first_gui.py
+++ my_first_gui.py
@@ -17,16 +17,24 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 #%% S4 - Create model
 
 #%% S5 - Create GUI Elements
+# Window creation
 root = tk.Tk()
 root.geometry("1000x750")
 root.resizable(False, False)
 root.title('My First GUI')
 
+#Subwindow creation
 figure1 = plt.Figure(figsize=(4,4), dpi=150)
 canvas = FigureCanvasTkAgg(figure1, master=root)
 canvas_widget = canvas.get_tk_widget()
+
+# Slider
+slider = tk.Scale(master=root, from_=10,to=180,label="Joint angle",resolution=1,length=200,orient='horizontal')
 #%% S6 - Place GUI Elements
+# Subvenster
 canvas_widget.pack(side="right", fill="y")
 
+# Slider
+slider.pack(side="bottom", pady=100)
 #%% S7- Create event loop
 root.mainloop()
