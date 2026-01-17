29c4181c50b887b2c5f6a86ab54f5760466458b9
Laurens van der Drift <Lauwy222@hotmail.com>
2026-01-17 14:37:35 +0100

    Add muscle length calculation and GUI labels
    Introduces calculation of muscle length based on arm geometry and displays the value in the GUI. Adds a logo and labels for muscle length, updates plotting to show the muscle, and refactors code for clarity and maintainability.

--- my_first_gui.py
+++ my_first_gui.py
@@ -11,57 +11,80 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 
-#%% S2 - Initial values
+#%% S2 - Initial values / constanten
 initial_elbow_ang = 60
 rad_to_deg = np.pi / 180
 
+fore_arm = 40      # lengte onderarm
+ins_dist = 4       # insertieafstand vanaf elleboog (cm)
+origo_y = 30       # origo op bovenarm: 30 cm boven elleboog
+
 #%% S3 - Gui Callback
 def slider_changed(*_):
-    # bij elke sliderbeweging model opnieuw tekenen
     calculate_model()
 
 #%% S4 - Create model
 def calculate_model():
-    # vaste punten / lengtes
+    # vaste punten
     elbow = [0, 0]
     shoulder = [0, 40]
-    fore_arm = 40
+    origo = [0, origo_y]   # op bovenarm
 
-    # actuele hoek uit slider
+    # actuele hoek
     elbow_ang = slider.get()
     segment_ang = 90 - elbow_ang
 
-    # bereken handpositie met sin/cos
-    dist_x = np.cos(segment_ang * rad_to_deg) * fore_arm
-    dist_y = np.sin(segment_ang * rad_to_deg) * fore_arm
-    hand = [elbow[0] + dist_x, elbow[1] + dist_y]
+    # hand (einde onderarm)
+    dist_x_hand = np.cos(segment_ang * rad_to_deg) * fore_arm
+    dist_y_hand = np.sin(segment_ang * rad_to_deg) * fore_arm
+    hand = [elbow[0] + dist_x_hand, elbow[1] + dist_y_hand]
+
+    # insertiepunt spier (op onderarm, 4 cm vanaf elleboog)
+    dist_x_ins = np.cos(segment_ang * rad_to_deg) * ins_dist
+    dist_y_ins = np.sin(segment_ang * rad_to_deg) * ins_dist
+    insertion = [elbow[0] + dist_x_ins, elbow[1] + dist_y_ins]
+
+    # spierlengte (pythagoras)
+    musc_vect = [origo[0] - insertion[0], origo[1] - insertion[1]]
+    musc_length = np.sqrt(musc_vect[0]**2 + musc_vect[1]**2)
+    musc_length = round(musc_length, 2)
+
+    # update label
+    muscval_lbl.config(text=musc_length)
 
     # plot
     figure1.clear()
     ax1 = figure1.add_subplot()
 
-    # bovenarm: shoulder -> elbow
+    # bovenarm
     ax1.plot([shoulder[0], elbow[0]], [shoulder[1], elbow[1]], color="blue")
-
-    # onderarm: elbow -> hand
+    # onderarm
     ax1.plot([elbow[0], hand[0]], [elbow[1], hand[1]], color="blue")
+    # spier
+    ax1.plot([origo[0], insertion[0]], [origo[1], insertion[1]], color="red")
 
     ax1.set(xlim=(-10, 60), ylim=(-60, 60))
     ax1.set_aspect('equal')
-
     canvas.draw()
 
 #%% S5 - Create GUI Elements
-# Window creation
 root = tk.Tk()
 root.geometry("1000x750")
 root.resizable(False, False)
 root.title('My First GUI')
 
-# Subwindow creation (matplotlib in tkinter)
-figure1 = plt.Figure(figsize=(4, 4), dpi=150, facecolor='#F0F0F0')
+# Plot in tkinter
+figure1 = plt.Figure(figsize=(4, 4), dpi=150, facecolor="#d9d9d9")
 canvas = FigureCanvasTkAgg(figure1, master=root)
 canvas_widget = canvas.get_tk_widget()
+
+# Logo (zorg dat btlogo.png in dezelfde map staat)
+pict = tk.PhotoImage(file="asset/btlogo.png")
+pict_lbl = tk.Label(master=root, image=pict)
+
+# Labels (spierlengte)
+musc_lbl = tk.Label(master=root, text="Muscle length")
+muscval_lbl = tk.Label(master=root, text="", bg="white", width=10)
 
 # Slider
 slider = tk.Scale(
@@ -75,12 +98,17 @@ slider = tk.Scale(
 )
 slider.set(initial_elbow_ang)
 
-# teken direct 1e keer (startpositie)
-calculate_model()
-
 #%% S6 - Place GUI Elements
 canvas_widget.pack(side="right", fill="y")
+
+# links (volgorde = van boven naar beneden)
+pict_lbl.pack(side="top", pady=100)
+musc_lbl.pack(side="top", pady=(10, 5))
+muscval_lbl.pack(side="top", pady=(0, 40))
 slider.pack(side="bottom", pady=100)
+
+# start-tekening
+calculate_model()
 
 #%% S7 - Create event loop
 root.mainloop()
